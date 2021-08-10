97f007d946fa33490e738f8cfe7ff89ac64a83e7
muradreza <88139954+muradreza@users.noreply.github.com>
2021-08-10 18:47:54 +0600

    Add files via upload

--- p.py
+++ p.py
@@ -1,8 +1,34 @@
 
-for i in range(10):
-   k=str(i)
-   a="g.txt"
-   c=k+a
+import random
+
+lent=1
+  
+  
+
+
+for i in range(3):
+   st=str(i)
+   tx=".txt"
+   a="bcdfghjklmnpqrstvwxz"
+   b="aeiouy"
+   rm1=random.sample(a,lent)
+   rm2=random.sample(b,lent)
+   rm3=random.sample(a,lent)
+   rm4=random.sample(b,lent)
+   rm5=random.sample(a,lent)
+   rm6=random.sample(b,lent)
+   rm7=random.sample(a,lent)
+   rm8=random.sample(b,lent)
+
+   word="".join(rm1+rm2+rm3+rm4+rm5+rm6+rm7+rm8)
+
+   c=word+st+tx
+   
    f= open(c,"w")
-   f.write(c+" hack your vsd")
+   f.write(word+" hack your vsd")
+   
    print(c)
+   
+
+else :
+  m= input()
